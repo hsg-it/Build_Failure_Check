f22783756784a011c3c1ad531c15a38efc31bd47
Jalendar Gummadi <jalendar@aizatron.com>
2023-01-19 19:14:29 +0200

    Basic script

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+import logging
+
+if __name__ == "__main__":
+    print("inside main")
+    url = "https://10.72.179.214:8443/api/v01/auth/login"
+
+    try:
+        res = requests.post(url)
+        response = res.json()
+        logging.info(f"Sign in to NIMS successful")
+    
+    except Exception as e:
+        # print("Error :",e)
+        logging.error(f"Error during NIMS sign in: {e}")
